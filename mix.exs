4bba99a910b0c715bd8c05180a19830de140035c
Bryce Darling <bryce@visdocs.com>
2019-01-23 12:44:06 -0500

    Replace cowboy with plug_cowboy dep

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Pictures.Mixfile do
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
+      {:plug_cowboy, "~> 1.0"},
       {:absinthe, "~> 1.4"},
       {:absinthe_plug, "~> 1.4"}
    ]
