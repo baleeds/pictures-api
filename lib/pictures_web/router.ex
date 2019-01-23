b80ae2e4c5e7f4f9a891c3fac68c4c3f1c92b3ba
Bryce Darling <bryce@visdocs.com>
2019-01-23 12:44:48 -0500

    Add graphql and graphiql routes

--- lib/pictures_web/router.ex
+++ lib/pictures_web/router.ex
@@ -5,7 +5,20 @@ defmodule PicturesWeb.Router do
     plug(:accepts, ["json"])
   end
 
-  scope "/api", PicturesWeb do
+  scope "/graphql" do
     pipe_through(:api)
+
+    forward("/", Absinthe.Plug, schema: PicturesWeb.Schema)
+  end
+
+  scope "/graphiql" do
+    pipe_through(:api)
+
+    forward(
+      "/",
+      Absinthe.Plug.GraphiQL,
+      schema: PicturesWeb.Schema,
+      interface: :playground
+    )
   end
 end
