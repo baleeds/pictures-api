7c297fc7726b5ac86f4e874cf01dc575197625b5
Bryce Darling <bryce@visdocs.com>
2019-01-23 12:44:38 -0500

    Seed pictures data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,34 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Pictures.Repo.insert!(%Pictures.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Pictures.{Repo, Picture}
+
+Repo.insert!(%Picture{
+  name: "Rainbow monsters",
+  url: "https://static.familyjr.ca/rendition/4293/1058/595",
+  size: 89107
+})
+
+Repo.insert!(%Picture{
+  name: "Construction crew",
+  url:
+    "https://vignette.wikia.nocookie.net/spongebob/images/2/22/My_Pretty_Seahorse_19.jpg/revision/latest?cb=20111015224125",
+  size: 105_706
+})
+
+Repo.insert!(%Picture{
+  name: "I'll be back",
+  url:
+    "https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2018/01/the_terminator_still.jpg",
+  size: 47862
+})
+
+Repo.insert!(%Picture{
+  name: "Harry Spotter",
+  url: "https://pics.me.me/harry-spotter-do-you-even-leviosa-um-why-tho-39540925.png",
+  size: 149_847
+})
+
+Repo.insert!(%Picture{
+  name: "Lit",
+  url:
+    "https://www.awn.com/sites/default/files/styles/original/public/image/featured/1027753-tom-and-jerry-blamed-mid-east-violence.jpg?itok=Hyqev1zp",
+  size: 77598
+})
